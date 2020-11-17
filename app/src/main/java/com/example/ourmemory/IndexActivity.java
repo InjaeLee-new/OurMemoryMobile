3b1d8aa7b329841b341c82212418f0955fa8f18d
InjaeLee-new <eij9404@naver.com>
2020-11-17 17:01:05 +0900

    각자 맡은 바를 주석으로 설명을 적어놓음. MainActivity와 IndexActivity를 확인하면 충분할듯.

--- app/src/main/java/com/example/ourmemory/IndexActivity.java
+++ app/src/main/java/com/example/ourmemory/IndexActivity.java
@@ -9,6 +9,11 @@ import android.widget.Button;
 
 public class IndexActivity extends AppCompatActivity implements View.OnClickListener {
 
+    /**
+     * 하단에 onClickListener 부분에 주석으로 해야할 내용들을 남겨놨습니다.
+     * 아래쪽으로 가서 확인해주세요.
+     */
+
     Button buttonToMemoryList;
 
     Button buttonSI , buttonSW, buttonSB;
@@ -40,11 +45,14 @@ public class IndexActivity extends AppCompatActivity implements View.OnClickList
                 startActivity(intent);
                 break;
 
-            /** 각자 필요한 인텐트까지만해서 작업해주면돼 크게 코드는 이렇게 구분할 수 있어
+            /** 각자 필요한 인텐트까지만해서 작업해주면돼요. 크게 코드는 이렇게 구분됩니다.
              *  Intent intent = new Intent(시작 엑티비티 , 이동할 엑티비티);
              *  startActivity(intent);
              *  일반적으로 시작 엑티비티는 this로 현재 엑티비티를 설정하면 되는거고
-             *  이동할 엑티비티는 본인들이 만든 내용으로 처리한다.
+             *  이동할 엑티비티는 본인들이 만든 액티비티로 이동시키면 됩니다.
+             *
+             *  위의 코드를 확인하면 buttonToMemoryList 버튼에 대한 이벤트 처리가 되어있는데,
+             *  해당 코드를 참고해서 아래 case를 자기꺼에 맡게 처리하시면 됩니다.
              */
 
             case R.id.buttonSI:
--- app/src/main/java/com/example/ourmemory/MainActivity.java
+++ app/src/main/java/com/example/ourmemory/MainActivity.java
@@ -18,6 +18,22 @@ import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    /**
+     * 메인 엑티비티에서 다른 버튼들은 아직 미구현.
+     * 메인에 진입했을 때 로그인하기 버튼을 눌러서
+     * 로그인 창이 뜨면 암거나 입력해도 상관없습니다.
+     *
+     * 1. 메인 엑티비티에서 로그인 버튼을 클릭하면 현재 메인페이지에 로티 파일은 지워지고
+     *    로그인 창이 동작합니다.
+     * 2. 로그인 기능을 미리 구현하면, 여러분들이 받고나서 데이터 처리가 애매해 일부러 처리 안했습니다.
+     *
+     * 3. 아무거나 맞춰서 로그인하면 제가 작업하라고 만들어 놓은 파일이 있습니다.
+     *
+     * 4. 현재 리스트버튼을 누르면 서버 연동이 안되서 아마 여러분들 환경에선 실행이 안될겁니다.
+     *
+     * 5. 대신 각자 한명씩 버튼을 만들어 놨는데. 자세한 내용은 IndexActivity에서 확인하시면 됩니다.
+     */
+
     LinearLayout layoutMain, layoutSecond, login_form, main_form;
     LottieAnimationView lottie1, lottie2, lottie3;
 
