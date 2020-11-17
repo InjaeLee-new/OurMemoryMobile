a7ccc549a239ea9fba2d07798cfcdc2be6c8e28f
InjaeLee-new <eij9404@naver.com>
2020-11-17 15:59:46 +0900

    팀프로젝트를 위한 수정 완료.

--- app/src/main/java/com/example/ourmemory/IndexActivity.java
+++ app/src/main/java/com/example/ourmemory/IndexActivity.java
@@ -11,6 +11,8 @@ public class IndexActivity extends AppCompatActivity implements View.OnClickList
 
     Button buttonToMemoryList;
 
+    Button buttonSI , buttonSW, buttonSB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,13 +20,34 @@ public class IndexActivity extends AppCompatActivity implements View.OnClickList
 
         buttonToMemoryList = findViewById(R.id.buttonToMemoryList);
 
+        buttonSI = findViewById(R.id.buttonSI);
+        buttonSW = findViewById(R.id.buttonSW);
+        buttonSB = findViewById(R.id.buttonSB);
+
+
         buttonToMemoryList.setOnClickListener(this);
+        buttonSI.setOnClickListener(this);
+        buttonSW.setOnClickListener(this);
+        buttonSB.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(this, ListActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-        startActivity(intent);
+        switch (v.getId()) {
+            case R.id.buttonToMemoryList:
+                Intent intent = new Intent(this, ListActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+                startActivity(intent);
+                break;
+            case R.id.buttonSI:
+                // 성인누나가 새롭게 Activity 만들어서 여기서 이동시키면돼.
+                break;
+            case R.id.buttonSW:
+                // 승원이도 하나 Activity 만들어서 이동시켜주자.
+                break;
+            case R.id.buttonSB:
+                // 세번쓰기 힘들다 성빈아 만들어서 이동시켜주자.
+                break;
+        }
     }
 }
