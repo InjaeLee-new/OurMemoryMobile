b38927f9187404783e2134b6cc8ff7c9136bfe78
InjaeLee-new <eij9404@naver.com>
2020-11-20 13:14:01 +0900

    구글 로그인 구현 현재 정보까진 받아올 수 있음

--- app/src/main/java/com/example/ourmemory/MainActivity.java
+++ app/src/main/java/com/example/ourmemory/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.ourmemory;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.Animator;
@@ -13,8 +15,22 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -43,11 +59,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     Boolean LoginOK = false;
 
-    @Override
+    // 구글 로그인을 위해 작성
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient; //  만약 임포트가 안되면 sync가 제대로 되지 않은것.
+//    Button google_signin;
+    SignInButton google_signin;
+
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 구글 로그인을 위해 작성
+       GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+               .requestIdToken(getString(R.string.default_web_client_id))
+               .requestEmail()
+               .build(); // 구글 사인인 버튼을 누를 때 기본적인 옵션 정리 코드
+       mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+       mAuth = FirebaseAuth.getInstance(); // 파이어 베이스 인증 객체 초기화
+       google_signin = (SignInButton) findViewById(R.id.sign_in_button);
+       google_signin.setOnClickListener(this);
+
+       // 일반 코드 시작
         login_form = findViewById(R.id.login_form);
         main_form = findViewById(R.id.main_form);
 
@@ -57,6 +90,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonJoin = findViewById(R.id.buttonJoin);
         buttonLoginOK = findViewById(R.id.buttonLoginOK);
         buttonToMain = findViewById(R.id.buttonToMain);
+
 
 
         buttonLogin.setOnClickListener(this);
@@ -106,6 +140,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    //구글 로그인 버튼 클릭이벤트도 추가해서 클릭시 아래 함수 호출.
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 1); // 로그인 화면 갔다가 인증 후 다시 돌아올 때 결과값 확인용
+        Toast.makeText(this, "signIn 함수 동작완료", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     public void onClick(View v) {
         //에니메이션 로드
@@ -136,6 +177,95 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 main_form.setVisibility(View.VISIBLE);
                 break;
 
+            case R.id.sign_in_button:
+                signIn();
+                break;
+
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Toast.makeText(this, "signIn 함수에서 나온 requestCode = " + requestCode, Toast.LENGTH_SHORT).show();
+        if (requestCode == 1) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            Toast.makeText(this, "task 가져왔어 task = " + task, Toast.LENGTH_SHORT).show();
+            //data는 로그인 후 가져온 데이터
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Toast.makeText(this, "account 가져왔어 account = " + account, Toast.LENGTH_SHORT).show();
+                // account <= 구글로그인 정보를 담고 있음
+                firebaseAuthWithGoogle(account.getIdToken()); // 로그인 결과값 출력하라는 함수
+            } catch (ApiException e) {
+                Toast.makeText(this, "account도 못가져왔어", Toast.LENGTH_SHORT).show();
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() { // 로그인의 실제 성공했는지!
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) { // 로그인이 성공했으면.
+                            Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_SHORT).show();
+                            // Sign in success, update UI with the signed-in user's information
+                            // FirebaseUser = 사용자 프로필을 조작하고 인증 공급자에 연결하거나 연결을 끊고 인증 토큰을 새로 고칠 수 있음
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);// 함수 호출
+                        } else {                    // 로그인이 실패했으면
+                            // If sign in fails, display a message to the user.
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    // updateUI(user) 함수 작성 ( 실제 화면 결과화면 이동 후 추가 작성, 또는 사용자 정보 가져오게)
+
+    private void updateUI(FirebaseUser currentUser) {
+        Toast.makeText(this, "업데이트UI 함수 동작", Toast.LENGTH_SHORT).show();
+        if(currentUser != null){
+            Toast.makeText(this, "받아온 정보가 있어!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+            intent.putExtra("nickname", currentUser.getDisplayName()); 		// 보여지는 이름
+            //url을 string 형식으로 넘겨야 에러가 안 뜸
+            intent.putExtra("photoUrl", String.valueOf(currentUser.getPhotoUrl()));	// 프로필 사진 url
+            intent.putExtra("email",currentUser.getEmail());			// 이메일
+            intent.putExtra("phoneNumber",currentUser.getPhoneNumber());	//전화번호 전달 받기(안 받아짐)
+            intent.putExtra("providerId",currentUser.getProviderId());		// firebase 전달 받기
+            intent.putExtra("tenantId",currentUser.getTenantId());			// null로 옴 전달 받기
+            intent.putExtra("uid",currentUser.getUid());				// 구글에서 정한 user id 인듯
+            startActivity(intent);
+        } else {
+            Toast.makeText(this,"실패",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /*
+     그 후에 넘겨받는 곳에서 glide 생성 후 이미지 또는 정보 출력 및 저장 (onCreate에 작성)
+
+        Intent intent = getIntent();
+        String nickname = intent.getStringExtra("nickname");        // 닉네임 전달 받기
+        String email = intent.getStringExtra("email");              // 이메일 전달 받기
+        String phoneNumber = intent.getStringExtra("phoneNumber");  // 전화번호 전달 받기(안 받아짐)
+        String providerId = intent.getStringExtra("providerId");    // firebase 전달 받기
+        String tenantId = intent.getStringExtra("tenantId");        // null로 옴 전달 받기
+        String photoUrl = intent.getStringExtra("photoUrl");        // 사진 전달 받기
+        String uid = intent.getStringExtra("uid");        // 사진 전달 받기
+
+        tv_result = findViewById(R.id.tv_result);
+        iv_profile = findViewById(R.id.iv_profile);
+
+        tv_result.setText("이름 : " +nickname + "\nuid : "+ uid
+                +"\nemail : "+email+"\nphoneNumber : "+phoneNumber+
+                "\nproviderId : "+providerId +"\ntenantId : "+tenantId
+                    );
+        Glide.with(this).load(photoUrl).into(iv_profile); // 프로필 url을 세팅
+     */
 }
---
+++ app/src/main/java/com/example/ourmemory/ResultActivity.java
@@ -0,0 +1,41 @@
+package com.example.ourmemory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class ResultActivity extends AppCompatActivity {
+
+    TextView result;
+    ImageView profile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        String nickname = intent.getStringExtra("nickname");        // 닉네임 전달 받기
+        String email = intent.getStringExtra("email");              // 이메일 전달 받기
+        String phoneNumber = intent.getStringExtra("phoneNumber");  // 전화번호 전달 받기(안 받아짐)
+        String providerId = intent.getStringExtra("providerId");    // firebase 전달 받기
+        String tenantId = intent.getStringExtra("tenantId");        // null로 옴 전달 받기
+        String photoUrl = intent.getStringExtra("photoUrl");        // 사진 전달 받기
+        String uid = intent.getStringExtra("uid");        // 사진 전달 받기
+
+        result = findViewById(R.id.resultString);
+        profile = findViewById(R.id.resultProfile);
+
+        result.setText("이름 : " +nickname + "\nuid : "+ uid
+                +"\nemail : "+email+"\nphoneNumber : "+phoneNumber+
+                "\nproviderId : "+providerId +"\ntenantId : "+tenantId
+        );
+        Glide.with(this).load(photoUrl).into(profile); // 프로필 url을 세팅
+    }
+}
